062fb841f466f1172e36ca295234c29b406cb344
adonathan <a3davis1400@gmail.com>
2025-06-16 11:25:37 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,242 @@
+var members = [
+  {
+    name: "Darcy Higginbotham ",
+    position: "Lead PHES",
+    email: "Darcy.higginbotham@lpha.mo.gov",
+    expertise: "Plan Review,Emergency response,HACCP,Foodborne outbreak investigations,Lodging inspections,Retail food inspections",
+    lat: 38.0326,
+    lon: -92.7646
+  },
+  {
+    name: "Deb Sees",
+    position: "Director",
+    email: "Dsees@jacksongov.org",
+    expertise: "nan",
+    lat: 39.0169,
+    lon: -94.3565
+  },
+  {
+    name: "Emily Wright",
+    position: "EPHS",
+    email: "emily.wright@lpha.mo.gov",
+    expertise: "nan",
+    lat: 36.9265,
+    lon: -90.9535
+  },
+  {
+    name: "Guy Gerard",
+    position: "Environmental Specialist/Preparedness Coordinator",
+    email: "ggerard@webstercohealth.com",
+    expertise: "nan",
+    lat: 37.2594,
+    lon: -92.8576
+  },
+  {
+    name: "Jack Hunsucker",
+    position: "EPHS/PHEP Planner",
+    email: "jackhunsucker@nodawayhealth.org",
+    expertise: "Licensed Advanced OWTS Installer,Childcare inspections,Lodging inspections,Retail food inspections",
+    lat: 40.3601,
+    lon: -94.8221
+  },
+  {
+    name: "Jacklyn Skaggs ",
+    position: "EPH Training Manager ",
+    email: "Jacklyn.skaggs@kcmo.org",
+    expertise: "Emergency response,HACCP,Foodborne outbreak investigations,Food handler/manager class instructor,Data analysis,Training Standard",
+    lat: 39.0997,
+    lon: -94.5786
+  },
+  {
+    name: "Jacob Williams",
+    position: "Environmental Public Health Specialist",
+    email: "Jacob.Williams@Lpha.mo.gov",
+    expertise: "nan",
+    lat: 36.6547,
+    lon: -93.0
+  },
+  {
+    name: "Jennifer C Miller",
+    position: "Public Health Environmental Specialist/WIC Nutrition Coordinator",
+    email: "jennifer.miller@lpha.mo.gov",
+    expertise: "Hand hygiene,AIDET Communication Framework,SDS Coordinator",
+    lat: 37.9656,
+    lon: -90.0424
+  },
+  {
+    name: "Jimmy LaChance ",
+    position: "EPHS Supervisor ",
+    email: "Jlachance@randolphcountyhealth.org",
+    expertise: "Plan Review proctor",
+    lat: 39.438,
+    lon: -92.5
+  },
+  {
+    name: "Karen Clifton ",
+    position: "PHES",
+    email: "Karen.Clifton@capecountyhealth.com",
+    expertise: "Retail Standards grant coordinator,Servsafe proctor,State Food Safety proctor",
+    lat: 37.3059,
+    lon: -89.5181
+  },
+  {
+    name: "Kathryn Rhodes ",
+    position: "Program support coordinator ",
+    email: "Kathryn.rhodes@jeffcohealth.org",
+    expertise: "nan",
+    lat: 38.2864,
+    lon: -90.6226
+  },
+  {
+    name: "Leah Ferris",
+    position: "Program Manager ",
+    email: "Lferris@clayhealth.com",
+    expertise: "Plan Review,HACCP,Foodborne outbreak investigations,Conflict resolution",
+    lat: 39.3178,
+    lon: -94.4197
+  },
+  {
+    name: "Lisa Brenneman",
+    position: "EPHS",
+    email: "Lbrenneman@springnfieldmo.gov",
+    expertise: "Plan Review proctor",
+    lat: 37.2089,
+    lon: -93.2923
+  },
+  {
+    name: "Mark Snyder",
+    position: "Environmental Health",
+    email: "msnyder@maconmohealth.org",
+    expertise: "Plan Review,Emergency response,Foodborne outbreak investigations",
+    lat: 39.803,
+    lon: -92.528
+  },
+  {
+    name: "Monica Roth",
+    position: "Environmental Supervisor and training coordinator ",
+    email: "Mroth@stlouiscountymo.gov",
+    expertise: "HACCP,Foodborne outbreak investigations,Food handler/manager class instructor",
+    lat: 38.61,
+    lon: -90.412
+  },
+  {
+    name: "Paige Anderson",
+    position: "Retail food program manager ",
+    email: "Paige.anderson@health.mo.gov",
+    expertise: "Emergency response,HACCP,Foodborne outbreak investigations,Recalls",
+    lat: 38.5767,
+    lon: -92.1735
+  },
+  {
+    name: "Peter Tirop",
+    position: "EPHS",
+    email: "Ptirop@stjosephmo.gov",
+    expertise: "Plan Review,Emergency response,HACCP,Foodborne outbreak investigations",
+    lat: 39.7675,
+    lon: -94.8466
+  },
+  {
+    name: "Rachel Cooper",
+    position: "Environmental Public Health Specialist",
+    email: "rachel.cooper@jeffcohealth.org",
+    expertise: "Plan Review,Servsafe proctor,Emergency response,Foodborne outbreak investigations,Food handler/manager class instructor,Childcare inspections",
+    lat: 38.2864,
+    lon: -90.6226
+  },
+  {
+    name: "Randy Strough",
+    position: "Environmental Public Health Specialist",
+    email: "randy.strough@coopercohealth.gov",
+    expertise: "nan",
+    lat: 38.8447,
+    lon: -92.82
+  },
+  {
+    name: "Robert Gilliland",
+    position: "Section Chief Environmental Health Protection",
+    email: "rgilliland@clayhealth.com",
+    expertise: "SAVA Certification,Plan Review,HACCP,Foodborne outbreak investigations,Risk Factor Study,Training Standard",
+    lat: 39.3178,
+    lon: -94.4197
+  },
+  {
+    name: "Sarah Vaughn",
+    position: "Public Health Specialist 3",
+    email: "sarah.vaughn@jacksongov.org",
+    expertise: "HACCP,Food handler/manager class instructor,Retail food inspections",
+    lat: 39.0169,
+    lon: -94.3565
+  },
+  {
+    name: "Shannon Franklin ",
+    position: "Food/Environmental Manager ",
+    email: "Sfranklin@stlouiscountymo.gov ",
+    expertise: "Emergency response,HACCP,Foodborne outbreak investigations,Risk Factor Study",
+    lat: 38.61,
+    lon: -90.412
+  },
+  {
+    name: "Stefanie Davis",
+    position: "Environmentalist/Emergency Planner ",
+    email: "Sdavis@pikecountyhealth.org ",
+    expertise: "nan",
+    lat: 39.3665,
+    lon: -91.1353
+  },
+  {
+    name: "Tarrah Thornhill ",
+    position: "Environmental Health Specialist ",
+    email: "tcthornhill@springfieldmo.gov",
+    expertise: "nan",
+    lat: 37.2089,
+    lon: -93.2923
+  },
+  {
+    name: "Tiffany Klassen ",
+    position: "Environmental Health supervisor ",
+    email: "tklassen@johnsoncountyhealth.org",
+    expertise: "Servsafe proctor,Emergency response",
+    lat: 38.7626,
+    lon: -93.8248
+  },
+  {
+    name: "Tom Flowers",
+    position: "Environmental Public Health Specialist ",
+    email: "tflowers@springfieldmo.gov",
+    expertise: "nan",
+    lat: 37.2089,
+    lon: -93.2923
+  },
+  {
+    name: "Trinidad Loza",
+    position: "EPHS",
+    email: "Trinidad.loza@lpha.mo.gov",
+    expertise: "Retail food inspections",
+    lat: 38.0326,
+    lon: -92.7646
+  },
+  {
+    name: "Violet Levitt",
+    position: "Public Health Specialist 1",
+    email: "Vlevitt@jacksongov.org",
+    expertise: "Retail food inspections",
+    lat: 39.0169,
+    lon: -94.3565
+  },
+  {
+    name: "Wes Crawford",
+    position: "Plan Review/EPHS",
+    email: "wcrawford@sccmo.org",
+    expertise: "Plan Review",
+    lat: 38.7885,
+    lon: -90.4976
+  },
+  {
+    name: "Zane Lonsway",
+    position: "Environmental Health Specialist I",
+    email: "zanel@casscounty.com",
+    expertise: "nan",
+    lat: 38.6573,
+    lon: -94.3515
+  },
+];
